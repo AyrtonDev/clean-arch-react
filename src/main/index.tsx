4716fbd8153989141a64839030c518740b222c8f
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-06-25 17:40:58 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-import { Login } from '@/presentation/pages'
+import { Router } from '@/presentation/components'
+import '@/presentation/styles/global.scss'
 
 const root = createRoot(document.getElementById('main'))
 
-root.render(<Login />)
+root.render(<Router />)
